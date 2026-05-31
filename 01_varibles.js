be6ac9108ac68714b9dcc91cf1d2133bc3d4aa29
shruti12-code <anushreya476@gmail.com>
2026-05-31 19:50:36 +0530

    Add files via upload

---
+++ 01_varibles.js
@@ -0,0 +1,16 @@
+const accountid = 1234
+let accountEmail = "hitesh@google.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+let accountstate;
+
+//accountid = 3455 \\not allowed
+accountEmail = "hc@hc.com"
+accountcity = "bengaluru"
+
+console.log(accountid);
+
+/*
+prefer not to use var
+because the issuse in block scope and functional scope*/
+console.table[accountid, accountEmail,accountpassword,accountcity,accountstate]
